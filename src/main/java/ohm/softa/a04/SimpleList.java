2999e78ffa06a4bbe53b92a8edf850556405a3b7
schumannsa67831 <schumannsa67831@TechnischeHochschuleN.onmicrosoft.com>
2019-04-12 13:17:34 +0200

    Part 1

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,10 +1,19 @@
 package ohm.softa.a04;
 
-public interface SimpleList extends Iterable {
+public interface SimpleList <T> extends Iterable <T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T o);
+
+	@SuppressWarnings("unchecked")
+	default void addDefault (Class <T> clazz){
+		try{
+			this.add(clazz.newInstance());
+		} catch (InstantiationException | IllegalAccessException e){
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * @return current size of the list
@@ -15,5 +24,21 @@ public interface SimpleList extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
+	@SuppressWarnings("unchecked")
+	default SimpleList <T> filter(SimpleFilter <T> filter) {
+		SimpleList<T> result;
+		try {
+			result = (SimpleList<T>) getClass().newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			result = new SimpleListImpl<>();
+		}
+
+		for (T o : this) {
+			if (filter.include(o)) {
+				result.add(o);
+			}
+		}
+
+		return result;
+	}
 }
